dd2df7e6411771173939a6dc4489b9adf7814ec4
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-02-27 20:01:20 +0530

    Add files via upload

---
+++ Basic Data Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a ;
+    long n ;
+    char c ;
+    float f ;
+    double d ;
+    cin >> a >> n >> c >> f >> d ;
+    printf ( "%d\n%ld\n%c\n%f\n%lf" , a , n , c , f , d ) ;
+    return 0;
+}
