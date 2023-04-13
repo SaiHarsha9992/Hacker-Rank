47e0a8d0ffca38c100bb2f39814d9659ffca51aa
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-04-13 21:49:39 +0530

    Add files via upload

---
+++ Java String Reverse.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String s = "";
+        for ( int i = 0 ; i < A.length() ; i++ )
+        {
+            s = A.charAt(i)+s;
+        }
+        if ( s.equals(A) )
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+        
+    }
+}
+
+
+
